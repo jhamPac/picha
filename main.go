7ce64b744995bb4ec1a8944d9d5003dae6518650
jhamPac <cloud@kevia.me>
2020-07-28 08:56:15 -0700

    created a custom route within one function

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Picha, share your photos securely</h2>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Home page</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "to get in touch, please send us an email "+"to <a href=\"mailto:support@picha.com\">"+"support@picha.com</a>")
+	}
 }
 
 func main() {
