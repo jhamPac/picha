8d5a75d0c4ef37b70a6a8f7275c7a464093201e6
jhamPac <cloud@kevia.me>
2020-07-28 09:51:58 -0700

    added a custom 404 route

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.NotFoundHandler = h
 
 	http.ListenAndServe(":9000", r)
 }
@@ -19,6 +20,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Picha, share photos securely!</h1>")
 }
+
+func notfound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>ARE YOU LOST?</h1>")
+}
+
+var h http.Handler = http.HandlerFunc(notfound)
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
