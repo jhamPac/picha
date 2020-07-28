d5055efcd8313bca041f2718626018dd90a26f4e
jhamPac <cloud@kevia.me>
2020-07-28 10:28:51 -0700

    contact route now using a template

--- main.go
+++ main.go
@@ -9,10 +9,16 @@ import (
 )
 
 var homeTemplate *template.Template
+var contactTemplate *template.Template
 
 func main() {
 	var err error
 	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
 	if err != nil {
 		panic(err)
 	}
@@ -41,5 +47,7 @@ var h http.Handler = http.HandlerFunc(notfound)
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "to get in touch, please send us an email "+"to <a href=\"mailto:support@picha.com\">"+"support@picha.com</a>")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
