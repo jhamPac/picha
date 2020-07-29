ed7173c40f45ee25789e25d84b6aaa78442d839b
jhamPac <cloud@kevia.me>
2020-07-29 07:15:18 -0700

    created a view package

---
+++ view/view.go
@@ -0,0 +1,21 @@
+package view
+
+import "html/template"
+
+// New instantiates a *View type and returns it
+func New(files ...string) *View {
+	files = append(files, "../templates/layouts/footer.gohtml")
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+		panic(err)
+	}
+
+	return &View{
+		Template: t,
+	}
+}
+
+// View represents a view created by combining n amount of templates
+type View struct {
+	Template *template.Template
+}
