301af524247a452a5df8163732c474e4732606bc
jhamPac <cloud@kevia.me>
2020-07-29 21:34:01 -0700

    created a glob helper function for view layouts

--- view/view.go
+++ view/view.go
@@ -1,14 +1,21 @@
 package view
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	// LayoutDir path to template layouts
+	LayoutDir string = "templates/layouts/"
+
+	// TemplateExt is the extension for templates
+	TemplateExt string = ".gohtml"
+)
 
 // New instantiates a *View type and returns it
 func New(layout string, files ...string) *View {
-	files = append(files,
-		"templates/layouts/appcontainer.gohtml",
-		"templates/layouts/navbar.gohtml",
-		"templates/layouts/footer.gohtml",
-	)
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
@@ -24,4 +31,12 @@ func New(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
