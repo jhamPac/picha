46653225f4bdbadbd4b88f59c9bd0bf09e78ef9e
jhamPac <cloud@kevia.me>
2020-08-01 09:52:03 -0700

    major cleanup by making view.View adhere to the http.Handler interface; ServeHTTP

--- controller/static.go
+++ controller/static.go
@@ -4,14 +4,14 @@ import "github.com/jhampac/picha/view"
 
 // Static represents all pages that renders static pages with no model bounded to them
 type Static struct {
-	HomeView    *view.View
-	ContactView *view.View
+	Home    *view.View
+	Contact *view.View
 }
 
 // NewStatic instantiates a *Static controller
 func NewStatic() *Static {
 	return &Static{
-		HomeView:    view.New("appcontainer", "templates/static/home.gohtml"),
-		ContactView: view.New("appcontainer", "templates/static/contact.gohtml"),
+		Home:    view.New("appcontainer", "templates/static/home.gohtml"),
+		Contact: view.New("appcontainer", "templates/static/contact.gohtml"),
 	}
 }
--- main.go
+++ main.go
@@ -15,28 +15,17 @@ var (
 )
 
 func main() {
-	homeView = view.New("appcontainer", "templates/home.gohtml")
-	contactView = view.New("appcontainer", "templates/contact.gohtml")
+	staticC := controller.NewStatic()
 	userC := controller.NewUser()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", home).Methods("GET")
-	r.HandleFunc("/contact", contact).Methods("GET")
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
 	r.HandleFunc("/signup", userC.New).Methods("GET")
 	r.HandleFunc("/signup", userC.Create).Methods("POST")
 	r.NotFoundHandler = h
 
 	http.ListenAndServe(":9000", r)
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
 }
 
 func notfound(w http.ResponseWriter, r *http.Request) {
@@ -45,10 +34,3 @@ func notfound(w http.ResponseWriter, r *http.Request) {
 }
 
 var h http.Handler = http.HandlerFunc(notfound)
-
-// must as in error must be nil
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
