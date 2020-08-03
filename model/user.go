1ab403a908a88d303b09f9ac34e3d8fb84feffb1
jhamPac <cloud@kevia.me>
2020-08-02 17:27:40 -0700

    abstracting away the db implementation

--- model/user.go
+++ model/user.go
@@ -1,10 +1,31 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
 
 // User represents our customers
 type User struct {
 	gorm.Model
 	Name  string
 	Email string `gorm:"not null;unique_index"`
+}
+
+// UserService is the DB abstraction layer
+type UserService struct {
+	db *gorm.DB
+}
+
+// NewUserService instantiates a new service with the provided connection information
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+	db.LogMode(true)
+	return &UserService{
+		db: db,
+	}, nil
 }
