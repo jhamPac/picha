39f81b1b9f4a0f5ef07a3cf93d86eb54b49974da
jhamPac <cloud@kevia.me>
2020-08-05 22:02:40 -0700

    Add Hash method to the HMAC type

--- hash/hmac.go
+++ hash/hmac.go
@@ -3,12 +3,21 @@ package hash
 import (
 	"crypto/hmac"
 	"crypto/sha256"
+	"encoding/base64"
 	"hash"
 )
 
 // HMAC is a custom wrapper for hmac
 type HMAC struct {
 	hmac hash.Hash
+}
+
+// Hash returns the base64 encoded string of the hmac result
+func (h HMAC) Hash(input string) string {
+	h.hmac.Reset()
+	h.hmac.Write([]byte(input))
+	b := h.hmac.Sum(nil)
+	return base64.URLEncoding.EncodeToString(b)
 }
 
 // NewHMAC instatiates a HMAC type with the provided key and sha256 as the function
