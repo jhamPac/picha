645087f974986add385653e5618148edca69451a
jhamPac <cloud@kevia.me>
2020-08-05 21:42:46 -0700

    Add String method to the rand package

--- rand/strings.go
+++ rand/strings.go
@@ -1,13 +1,25 @@
 package rand
 
-import "crypto/rand"
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
 
-// Byte generates n random bytes
-func Byte(n int) ([]byte, error) {
+// Bytes generates n random bytes
+func Bytes(n int) ([]byte, error) {
 	b := make([]byte, n)
 	_, err := rand.Read(b)
 	if err != nil {
 		return nil, err
 	}
 	return b, nil
+}
+
+// String generates a byte slice of size nBytes and then return an encoded string
+func String(nBytes int) (string, error) {
+	b, err := Bytes(nBytes)
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
 }
