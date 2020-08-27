9bdc6c438eb98206a7d5ba2d5ed9912b172c8641
jhamPac <cloud@kevia.me>
2020-08-26 20:46:24 -0700

    Implement validation for gallery data

--- model/gallery.go
+++ model/gallery.go
@@ -2,6 +2,11 @@ package model
 
 import "github.com/jinzhu/gorm"
 
+const (
+	ErrUserIDRequired modelError = "model: user ID is required"
+	ErrTitleRequired  modelError = "model: title is required"
+)
+
 // Gallery contains images to view
 type Gallery struct {
 	gorm.Model
@@ -31,10 +36,6 @@ type galleryGorm struct {
 	db *gorm.DB
 }
 
-func (gg *galleryGorm) Create(gallery *Gallery) error {
-	return gg.db.Create(gallery).Error
-}
-
 // NewGalleryService instantiates a new GalleryService
 func NewGalleryService(db *gorm.DB) GalleryService {
 	return &galleryService{
@@ -44,4 +45,43 @@ func NewGalleryService(db *gorm.DB) GalleryService {
 			},
 		},
 	}
+}
+
+func (gv *galleryValidator) Create(gallery *Gallery) error {
+	err := runGalleryValFns(gallery,
+		gv.userIDRequired,
+		gv.titleRequired)
+	if err != nil {
+		return err
+	}
+	return gv.GalleryDB.Create(gallery)
+}
+
+func (gg *galleryGorm) Create(gallery *Gallery) error {
+	return gg.db.Create(gallery).Error
+}
+
+type galleryValFn func(*Gallery) error
+
+func runGalleryValFns(gallery *Gallery, fns ...galleryValFn) error {
+	for _, fn := range fns {
+		if err := fn(gallery); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (gv *galleryValidator) userIDRequired(g *Gallery) error {
+	if g.UserID <= 0 {
+		return ErrUserIDRequired
+	}
+	return nil
+}
+
+func (gv *galleryValidator) titleRequired(g *Gallery) error {
+	if g.Title == "" {
+		return ErrTitleRequired
+	}
+	return nil
 }
