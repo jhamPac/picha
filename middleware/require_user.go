0404ffb0a10e46ba343cd2dfa8c140d09a908dbd
jhamPac <cloud@kevia.me>
2020-08-27 07:57:46 -0700

    Implement middleware for required users

--- middleware/require_user.go
+++ middleware/require_user.go
@@ -1,7 +1,33 @@
 package middleware
 
-import "github.com/jhampac/picha/model"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/jhampac/picha/model"
+)
 
 type RequireUser struct {
 	model.UserService
+}
+
+// ApplyFn chains to the next call
+func (mw *RequireUser) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("remember_token")
+		if err != nil {
+			http.Redirect(w, r, "/login", http.StatusNotFound)
+			return
+		}
+
+		user, err := mw.UserService.ByRemember(cookie.Value)
+		if err != nil {
+			http.Redirect(w, r, "/login", http.StatusNotFound)
+			return
+		}
+		fmt.Println("User found: ", user)
+
+		// pushes to next call
+		next(w, r)
+	})
 }
