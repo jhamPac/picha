19ec82f2d68d6093533f4767e5b31cea0ecc255b
jhamPac <cloud@kevia.me>
2020-08-30 15:16:50 -0700

    Add route to display galleries

--- controller/gallery.go
+++ controller/gallery.go
@@ -11,15 +11,17 @@ import (
 
 // Gallery controller for all related resources
 type Gallery struct {
-	NewView *view.View
-	gs      model.GalleryService
+	NewView  *view.View
+	ShowView *view.View
+	gs       model.GalleryService
 }
 
 // NewGallery instantiates a new controller for the gallery resource
 func NewGallery(gs model.GalleryService) *Gallery {
 	return &Gallery{
-		NewView: view.New("appcontainer", "gallery/new"),
-		gs:      gs,
+		NewView:  view.New("appcontainer", "gallery/new"),
+		ShowView: view.New("appcontainer", "gallery/show"),
+		gs:       gs,
 	}
 }
 
